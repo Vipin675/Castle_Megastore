b85441fe14ad264d5aeabb16dea218f1bab29f26
Vipin <vipinvishwa1m1@gmail.com>
2022-09-20 05:06:42 +0530

    updated category.selector.js to use reselect package (npm i reselect - it is an external library which provides memoization of our states so that while update any state the rest state shouldn't have the refetch of format the data again and re-render that component.)

--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -1,7 +1,20 @@
+import { createSelector } from "reselect";
+
+const selectCategoriesMapReducer = (state) => state.categories;
+
+export const selectCategories = createSelector(
+  [selectCategoriesMapReducer], // first argment is the item you want to store as memoized
+  (categoriesSlice) => categoriesSlice.categories // this will run only when the item get from useSelect is update or changed else the memoized item will passed
+);
+
 // The selector file is where this reducer specific transformation business logic is going to live.
-export const selectCategoriesMap = (state) =>
-  state.categories.categories.reduce((accumulator, docSnapshot) => {
-    const { title, items } = docSnapshot;
-    accumulator[title.toLowerCase()] = items;
-    return accumulator;
-  }, {});
+export const selectCategoriesMap = createSelector(
+  [selectCategories],
+
+  (categories) =>
+    categories.reduce((accumulator, docSnapshot) => {
+      const { title, items } = docSnapshot;
+      accumulator[title.toLowerCase()] = items;
+      return accumulator;
+    }, {})
+);
